fcbdb17468305f55ebe88bcc8321713d97e24e77
Maximilian Girlich <maximilian.girlich@metoda.com>
2024-04-30 13:08:05 +0200

    Add basic dockerfile

--- backend/main.py
+++ backend/main.py
@@ -1,9 +1,28 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 
 app = FastAPI()
 
+origins = [
+    'http://localhost:3000',
+    'http://localhost:5173',
+    'http://localhost:9000',
+]
 
-@app.get("/")
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+@app.get("/api/todos")
 def home():
-    return "Hello, World!"
+    return [
+        {"id": 1, "content": "a"},
+        {"id": 2, "content": "b"},
+        {"id": 3, "content": "c"},
+    ]
